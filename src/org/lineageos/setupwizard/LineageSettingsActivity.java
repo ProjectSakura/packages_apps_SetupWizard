crdroidandroid/android_packages_apps_SetupWizard

SetupWizard: Bring it on our side
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/org/lineageos/setupwizard/LineageSettingsActivity.java
@@ -52,7 +52,7 @@ public class LineageSettingsActivity extends BaseSetupWizardActivity {
 
     public static final String TAG = LineageSettingsActivity.class.getSimpleName();
 
-    public static final String PRIVACY_POLICY_URI = "http://lineageos.org/legal";
+    public static final String PRIVACY_POLICY_URI = "https://crdroid.net/legal.php";
 
     private SetupWizardApp mSetupWizardApp;
 

ProjectSakura/packages_apps_SetupWizard

Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
Change-Id: Ie2f5f075150cbcbcc011e3e50233016fef394b16

src/org/lineageos/setupwizard/LineageSettingsActivity.java
@@ -46,7 +46,7 @@ public class LineageSettingsActivity extends BaseSetupWizardActivity {
 
     public static final String TAG = LineageSettingsActivity.class.getSimpleName();
 
-    public static final String PRIVACY_POLICY_URI = "http://lineageos.org/legal";
+    public static final String PRIVACY_POLICY_URI = "https://projectsakura.xyz/index.html";
 
     private SetupWizardApp mSetupWizardApp;
 