LineageOS/android_packages_apps_SetupWizard

Fix various java warnings
Change-Id: I4c4381e1755831df2c4f41279800fd6af173e4ed

src/org/lineageos/setupwizard/FinishActivity.java
@@ -75,7 +75,7 @@ public class FinishActivity extends BaseSetupWizardActivity {
             if (LOGV) {
                 Log.v(TAG, "onReceive intent=" + intent);
             }
-            if (intent != null && intent.getAction() == ACTION_FINISHED) {
+            if (intent != null && intent.getAction().equals(ACTION_FINISHED)) {
                 unregisterReceiver(mIntentReceiver);
                 completeSetup();
             }
@@ -89,7 +89,7 @@ public class FinishActivity extends BaseSetupWizardActivity {
             logActivityState("onCreate savedInstanceState=" + savedInstanceState);
         }
         mSetupWizardApp = (SetupWizardApp) getApplication();
-        mReveal = (ImageView) findViewById(R.id.reveal);
+        mReveal = findViewById(R.id.reveal);
         setNextText(R.string.start);
     }
 
@@ -224,7 +224,7 @@ public class FinishActivity extends BaseSetupWizardActivity {
 
             try {
                 overlayManager.setEnabledExclusiveInCategory(selectedNavMode, USER_CURRENT);
-            } catch (Exception e) {
+            } catch (Exception ignored) {
             }
         }
     }

ProjectSakura/packages_apps_SetupWizard

src/org/lineageos/setupwizard/FinishActivity.java
@@ -81,7 +81,7 @@ public class FinishActivity extends BaseSetupWizardActivity {
             if (LOGV) {
                 Log.v(TAG, "onReceive intent=" + intent);
             }
-            if (intent != null && intent.getAction() == ACTION_FINISHED) {
+            if (intent != null && intent.getAction().equals(ACTION_FINISHED)) {
                 unregisterReceiver(mIntentReceiver);
                 completeSetup();
             }
@@ -96,7 +96,7 @@ public class FinishActivity extends BaseSetupWizardActivity {
             logActivityState("onCreate savedInstanceState=" + savedInstanceState);
         }
         mSetupWizardApp = (SetupWizardApp) getApplication();
-        mBackground = (ImageView) findViewById(R.id.background);
+        mBackground = findViewById(R.id.background);
         setNextText(R.string.start);
 
         // Edge-to-edge. Needed for the background view to fill the full screen.
@@ -231,7 +231,7 @@ public class FinishActivity extends BaseSetupWizardActivity {
 
             try {
                 overlayManager.setEnabledExclusiveInCategory(selectedNavMode, USER_CURRENT);
-            } catch (Exception e) {
+            } catch (Exception ignored) {
             }
         }
     }